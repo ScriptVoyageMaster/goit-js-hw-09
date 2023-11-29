746e0a0e3caf98fd2fa225d40be5c9c3d0cbaf92
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-11-30 01:53:27 +0200

    02-timer flatpickr notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,89 +1,82 @@
-// js/02-timer.js
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
 
-// Отримання елементів DOM
-const startButton = document.getElementById('startButton');
-const daysElement = document.querySelector('[data-days] .top-line');
-const hoursElement = document.querySelector('[data-hours] .top-line');
-const minutesElement = document.querySelector('[data-minutes] .top-line');
-const secondsElement = document.querySelector('[data-seconds] .top-line');
-const datetimePicker = document.getElementById('datetime-picker');
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        const selectedDate = selectedDates[0];
+        const currentDate = new Date();
+
+        if (selectedDate <= currentDate) {
+            Notiflix.Notify.failure('Please choose a date in the future');
+        } else {
+            updateStartButtonStatus();
+        }
+    },
+};
+
+flatpickr("#datetime-picker", options);
+
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
 
 let countdownInterval;
 
-// Встановлення початкових значень для datetimePicker
-setInitialDatetime();
-
-// Обробник зміни значення datetimePicker
-datetimePicker.addEventListener('change', () => {
-  updateStartButtonStatus();
-});
-
-// Функція для оновлення таймера
-function updateTimer(targetDate) {
-  const currentDate = new Date().getTime();
-  const timeDifference = targetDate - currentDate;
-
-  // Розрахунок днів, годин, хвилин та секунд
-  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-  // Оновлення значень на сторінці
-  daysElement.textContent = formatTimeValue(days);
-  hoursElement.textContent = formatTimeValue(hours);
-  minutesElement.textContent = formatTimeValue(minutes);
-  secondsElement.textContent = formatTimeValue(seconds);
-
-  // Перевірка, чи таймер досягнув нуля
-  if (timeDifference <= 0) {
-    clearInterval(countdownInterval); // Зупинка таймера
-    updateStartButtonStatus();        // Оновлення статусу кнопки "Start"
-  }
-}
-
-// Функція для форматування числових значень (додавання 0 перед числами < 10)
-function formatTimeValue(value) {
-  return value < 10 ? `0${value}` : value;
-}
-
-// Обробник натискання на кнопку "Start"
-startButton.addEventListener('click', () => {
-  // Отримання введеної дати та часу
-  const targetDate = new Date(datetimePicker.value).getTime();
-
-  // Перевірка, чи введена коректна дата
-  if (isNaN(targetDate)) {
-    alert('Please enter a valid date and time.');
-    return;
-  }
-
-  // Оновлення таймера кожну секунду
-  countdownInterval = setInterval(updateTimer, 1000, targetDate);
-
-  // Вимикаємо кнопку "Start"
-  startButton.disabled = true;
-});
-
-// Функція для оновлення статусу кнопки "Start"
 function updateStartButtonStatus() {
-  const selectedDate = new Date(datetimePicker.value).getTime();
-  const currentDate = new Date().getTime();
-
-  // Активація кнопки "Start" лише якщо вибрана дата та час більші за поточну системну
-  startButton.disabled = selectedDate <= currentDate;
+    startButton.disabled = false;
 }
 
-// Функція для встановлення початкових значень для datetimePicker
-function setInitialDatetime() {
-  const currentDatetime = new Date();
-  const year = currentDatetime.getFullYear();
-  const month = (currentDatetime.getMonth() + 1).toString().padStart(2, '0');
-  const day = currentDatetime.getDate().toString().padStart(2, '0');
-  const hours = currentDatetime.getHours().toString().padStart(2, '0');
-  const minutes = currentDatetime.getMinutes().toString().padStart(2, '0');
+function updateTimer(targetDate) {
+    const currentDate = new Date().getTime();
+    const timeDifference = targetDate - currentDate;
 
-  const initialDatetime = `${year}-${month}-${day}T${hours}:${minutes}`;
-  datetimePicker.value = initialDatetime;
-  updateStartButtonStatus(); // Оновлення статусу кнопки "Start"
+    if (timeDifference <= 0) {
+        clearInterval(countdownInterval);
+        updateStartButtonStatus();
+        return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
 }
+
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+    return value < 10 ? `0${value}` : value;
+}
+
+startButton.addEventListener('click', () => {
+    const targetDate = new Date(document.getElementById('datetime-picker').value).getTime();
+
+    if (isNaN(targetDate)) {
+        Notiflix.Notify.failure('Please enter a valid date and time.');
+        return;
+    }
+
+    countdownInterval = setInterval(updateTimer, 1000, targetDate);
+    startButton.disabled = true;
+});
